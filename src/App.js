ced5c949f4056fcaa7d503d6e1b0da2ceb362e84
Bharadwajpsb <saibharadwajpsb@gmail.com>
2022-11-17 20:51:32 +0530

    third

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import './App.css';
 import Nav from './components/nav/Nav';
 import Header from './components/header/Header';
 import Content from './components/content/Content';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://api.themoviedb.org/3';
+axios.defaults.params = {
+  api_key: "6454d89405181c054743fb941d955adf",
+};
+
 
 const App = () => {
   return (
